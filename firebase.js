d675a1ccb26561733c81968d19a95a6bac73992e
Jerald <70787522+Minehappy@users.noreply.github.com>
2021-12-01 14:22:46 +0800

    s

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD8KU1QJUxFCi30Qma5vzX4wxh_Rbhjwy4",
+    authDomain: "rm-staff-login.firebaseapp.com",
+    projectId: "rm-staff-login",
+    storageBucket: "rm-staff-login.appspot.com",
+    messagingSenderId: "256352558657",
+    appId: "1:256352558657:web:4bb3d331f13ffc2b6b70c0",
+    measurementId: "G-LHKKJ3HLHZ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
