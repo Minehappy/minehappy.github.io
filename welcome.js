9e9bafd20c7f6301cef67654fadc82c2efdda3cf
Jerald <70787522+Minehappy@users.noreply.github.com>
2021-12-01 16:45:18 +0800

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -2,7 +2,7 @@ firebase.auth().onAuthStateChanged((user)=>{
     if(!user){
         location.replace("index.html")
     }else{
-        document.getElementById("user").innerHTML = "Hello, <br style=text-align:center;/><h3>"+user.email
+        document.getElementById("user").innerHTML = "Hello, <br style=text-align:center; font-size:20vw;/>"+user.email
     }
 })
 
