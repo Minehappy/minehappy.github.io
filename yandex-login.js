1bc9f24074109d46701c364a52e12e8eb8f71f97
Jerald <70787522+Minehappy@users.noreply.github.com>
2021-12-02 11:43:40 +0800

    Website improvements

--- yandex-login.js
+++ yandex-login.js
@@ -1,14 +1,14 @@
-
-function yandex_email(){
-    const login = document.getElementById("yemail").value
-    const password = document.getElementById("ypassword").value
+fetch('https://pddimp.yandex.ru/api2/admin/email/add', {
+    method: 'POST',
+    mode: 'no-cors',
     
-    fetch('https://pddimp.yandex.ru/api2/admin/email/add', {
-     method: 'POST',
-        headers: {
-            'PddToken': 'IOIBTHWWB6EGBBQCSJ25CIUPTBKIR6WN5LN3NRZ7VVS26C4G3IOQ'
-      },
-      body: 'domain=robloxmemes.ml&login='+login+'&password='+password+''
-    });
-}
-
+    headers: {
+       'Content-Type': 'application/json',
+       'PddToken': 'IOIBTHWWB6EGBBQCSJ25CIUPTBKIR6WN5LN3NRZ7VVS26C4G3IOQ'
+     },
+     body: 'domain=robloxmemes.ml&login=hello@robloxmemes.ml&password=Jeraldgomia@2007'
+   }
+   .then(response => response.json())  // convert to json
+   .then(json => console.log(json))    //print data to console
+   .catch(err => console.log('Request Failed', err)) // Catch errors
+   );
