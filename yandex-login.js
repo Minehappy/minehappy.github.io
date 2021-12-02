f1415911a66c06d290aa5144f6a510c1e323e52d
Jerald <70787522+Minehappy@users.noreply.github.com>
2021-12-02 15:31:19 +0800

    .

--- yandex-login.js
+++ yandex-login.js
@@ -5,10 +5,11 @@ fetch('https://pddimp.yandex.ru/api2/admin/email/add', {
     headers: {
        'Content-Type': 'application/json',
        'PddToken': 'IOIBTHWWB6EGBBQCSJ25CIUPTBKIR6WN5LN3NRZ7VVS26C4G3IOQ'
+       .then(response => response.json())  // convert to json
+       .then(json => console.log(json))    //print data to console
+       .catch(err => console.log('Request Failed', err)) // Catch errors
      },
      body: 'domain=robloxmemes.ml&login=hello@robloxmemes.ml&password=Jeraldgomia@2007'
    }
-   .then(response => response.json())  // convert to json
-   .then(json => console.log(json))    //print data to console
-   .catch(err => console.log('Request Failed', err)) // Catch errors
+
    );
